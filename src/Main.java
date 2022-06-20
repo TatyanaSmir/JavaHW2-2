f6ed09b9403e30bbfc7d6d3874b09833c24e2f41
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-06-20 20:15:05 +0300

    Done

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;// на счету абонента
+        int y = 1000;// сумма пополнения счета
+
+        int bonus = 0;
+        if (y >= 1000){
+            bonus = (x + y) / 100;
+        }
+        int z = x + y + bonus;
+
+        System.out.println("Бонусный счет = " + bonus);
+        System.out.println("Баланс =" + z);
+
+        }
+
+
+    }
