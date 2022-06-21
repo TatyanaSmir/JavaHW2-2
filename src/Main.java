2839e2f650912a659f880bfb44badbe75d978c06
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-06-21 19:33:53 +0300

    Переименование переменных

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 10;// на счету абонента
-        int y = 1000;// сумма пополнения счета
+        int account1 = 10;// на счету абонента до пополнения счета
+        int refill = 1000;// сумма пополнения счета
 
         int bonus = 0;
-        if (y >= 1000) {
-            bonus = (x + y) / 100;
+        if (refill >= 1000) {
+            bonus = (account1 + refill) / 100;
         }
-        int z = x + y + bonus;
+        int account2 = account1 + refill + bonus;// на счету абонента после пополнения счета
 
         System.out.println("Бонусный счет = " + bonus);
-        System.out.println("Баланс =" + z);
+        System.out.println("Баланс =" + account2);
 
     }
 
