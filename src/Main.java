8a9a31e1153466a4c18695101f8773f0b469182c
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-06-21 20:01:56 +0300

    Корректировка условий начисления бонуса

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int account1 = 10;// на счету абонента до пополнения счета
-        int refill = 1000;// сумма пополнения счета
+        int refill = 1100;// сумма пополнения счета
 
         int bonus = 0;
-        if (refill >= 1000) {
+        if (refill > 1000) {
             bonus = (account1 + refill) / 100;
         }
         int account2 = account1 + refill + bonus;// на счету абонента после пополнения счета
